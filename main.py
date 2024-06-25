4c0bcc1b702a9c22825f83960c9e98d3cdab5880
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-06-25 14:28:17 -0400

    Add print_report function

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ def main():
     book_text = open_book('./books/frankenstein.txt')
     word_count = count_words(book_text)
     character_counts = count_characters(book_text)
+    character_list = convert_to_alphabet_list(character_counts)
+    character_list.sort(reverse=True,key=sort_by_count_callback)
 
-    print(book_text)
-    print(word_count)
-    print(character_counts)
+    print_report('./books/franktenstein.txt', word_count, character_list )
 
 def open_book(path):
     with open(path) as book:
@@ -24,5 +24,30 @@ def count_characters(text):
         character_counts[c] = character_counts.setdefault(c, 0) + 1
 
     return character_counts
+
+def convert_to_alphabet_list(dict):
+    characters = []
+
+    for c in dict:
+
+        if c.isalpha():
+            characters.append({
+                'name': c,
+                'count': dict[c]
+            })
+
+    return characters
+
+def sort_by_count_callback(dict):
+    return dict['count']
+
+def print_report(path, word_count, character_list):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document \n")
+
+    for character in character_list:
+        print(f"The {character['name']} character was found {character['count']} times.")
+
+    print('--- End report ---')
 
 main()
